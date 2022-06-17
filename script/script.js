df07bb4fd90e56d19bd6d7538ffeb85194645116
Caroline Vieira <caroline@fetch.ly>
2022-06-17 15:56:23 +0200

    restart game in progress

--- script/script.js
+++ script/script.js
@@ -3,6 +3,8 @@ const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
 const btnCoin = document.querySelector('.btn-coin');
 
+btnCoin.setAttribute('style', "display: none");
+
 const jump = ()=> {
     //adding a class jump
     mario.classList.add('jump');
@@ -11,13 +13,14 @@ const jump = ()=> {
         mario.classList.remove('jump');
 
     }, 500);
-
+    
+    const img = document.createElement("img");
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+    const pipePosition = pipe.offsetLeft;
     const loop = setInterval(()=> {
         //check pipe position
-        const pipePosition = pipe.offsetLeft;
 
         //mario position + convert to number
-        const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
     
         // small or equal to 120px and still display in screen and mario jumps is less than 80px stop animation
         if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80 ){
@@ -32,27 +35,36 @@ const jump = ()=> {
             mario.src = './images/game-over.png'
             mario.style.width = '75px'
             mario.style.marginLeft = '50px'
+
+            //background image game over
             setTimeout(()=> {
-                const img = document.createElement("img");
                 img.src = './images/game-over-final.png'
                 gameBoard.appendChild(img);
                 gameBoard.setAttribute('style', "position: absolute")
                 gameBoard.setAttribute('style', "text-align:center")
+                btnCoin.setAttribute('style', "display: block");
         
             }, 1000);
 
             //stop function when if finish
             clearInterval(loop)
-            btnCoin.addEventListener('click', jump);
         }
-
+        
     },10)
     
+    function test(){
+        console.log("here")
+        btnCoin.setAttribute('style', "display: none");
+        img.remove()
+        // pipe.style.animation = 'block';
+        // pipe.style.left = `${pipePosition}px`;
+
+        // mario.style.animation = 'block';
+        // mario.style.bottom = `${marioPosition}px`;
+        
+    }
+    btnCoin.addEventListener('click', test);
 }
 
 
-function test(){
-    console.log("here")
-
-}
 document.addEventListener('keydown', jump);
